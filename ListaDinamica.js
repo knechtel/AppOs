659b518c7c3255c037ca8294b17ecb74c069460d
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-05-24 13:24:10 -0300

    Adicionando lista de clients.

--- ListaDinamica.js
+++ ListaDinamica.js
@@ -1,19 +1,39 @@
 import React, { useState } from 'react';
-import { View, Text, FlatList, TextInput, Button, StyleSheet,  Animated } from 'react-native';
-
+import { useEffect } from "react";
+import {
+  View,
+  Text,
+  FlatList,
+  TextInput,
+  Button,
+  StyleSheet,
+  Animated,
+} from "react-native";
+import { FIND_ALL_CLIENT } from "./util/url";
 export default function ListaDinamica() {
+  const [list, setList] = useState([]);
   const [itens, setItens] = useState([
-    { id: '1', nome: 'Item 1' },
-    { id: '2', nome: 'Item 2' },
+    { id: "1", nome: "Item 1" },
+    { id: "2", nome: "Item 2" },
   ]);
+  useEffect(() => {
+    fetch(FIND_ALL_CLIENT) // Exemplo de API pública
+      .then((response) => response.json())
+      .then((json) => {
+        setList(json);
+      })
+      .catch((erro) => {
+        console.error("Erro ao buscar dados:", erro);
+      });
+  }, []);
 
-  const [novoItem, setNovoItem] = useState('');
+  const [novoItem, setNovoItem] = useState("");
 
-  const adicionarItem = () => {
-    if (novoItem.trim() === '') return;
+  const adicionarItem = (id) => {
+    if (novoItem.trim() === "") return;
     const novo = { id: Date.now().toString(), nome: novoItem };
     setItens([...itens, novo]);
-    setNovoItem('');
+    setNovoItem("");
   };
 
   return (
@@ -21,26 +41,27 @@ export default function ListaDinamica() {
       <Text style={styles.titulo}>Lista Dinâmica</Text>
 
       <FlatList
-        data={itens}
+        data={list}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
           <View style={styles.item}>
-            <Text>{item.nome}</Text>
+            <Text>{item.id + " " + item.name}</Text>
           </View>
         )}
       />
 
       <TextInput
-      style={styles.input}
+        style={styles.input}
         placeholder="Novo item"
         value={novoItem}
         onChangeText={setNovoItem}
       />
-<Button onPress={adicionarItem}
-            style={{ marginBottom: 50 }}
-            title="Adicionar equipamento"
-            color="#841584"
-          />
+      <Button
+        onPress={adicionarItem}
+        style={{ marginBottom: 50 }}
+        title="Adicionar equipamento"
+        color="#841584"
+      />
     </View>
   );
 }
