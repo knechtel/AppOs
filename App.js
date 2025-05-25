f19bf4a4ced01feea781ef4e12ab2bf459c95894
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-05-24 22:57:10 -0300

    Refatorando import.

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import ListaDinamica from "./ListaDinamica";
+import ListaDinamica from "./components/ListaDinamica";
 
 export default function App() {
   return <ListaDinamica />;
