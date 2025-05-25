2289d3acb46f9c05254f3583eb9225ac505304ec
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-05-25 17:45:44 -0300

    Adicionando lib de navegação.

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import ListaDinamica from "./components/ListaDinamica";
 
+const Stack = createStackNavigator();
 export default function App() {
-  return <ListaDinamica />;
+  return (
+    <NavigationContainer>
+      {/* <Stack.Navigator initialRouteName="Login"> */}
+      <ListaDinamica />;{/* </Stack.Navigator> */}
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
