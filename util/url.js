d8cb113fe632510361aca7a91acda24a42a41e41
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-07-12 18:56:58 -0300

    Adicionando urls.

--- util/url.js
+++ util/url.js
@@ -2,3 +2,5 @@ export const LOCAL =
   "http://www.sftcode.com";
 
 export const FIND_ALL_CLIENT = LOCAL + "/controller/client_controller_list.php";
+
+export const CREATE_EQUIPMENT = LOCAL+"/controller/equipment_controller_insert.php"
