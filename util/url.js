10825c41f13738f42977f95a7263ecedceb628bf
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-07-13 15:47:55 -0300

    Organizando urls.

--- util/url.js
+++ util/url.js
@@ -1,8 +1,8 @@
 export const LOCAL = "https://www.sftcode.com";
-
 export const FIND_ALL_CLIENT = LOCAL + "/controller/client_controller_list.php";
-
-export const CREATE_EQUIPMENT = LOCAL+"/controller/equipment_controller_insert.php"
-
+export const CREATE_EQUIPMENT =
+  LOCAL + "/controller/equipment_controller_insert.php";
 export const FIND_BY_ID_CLIENT =
   LOCAL + "/controller/client_controller_by_id.php";
+export const UPDATE_CLIENT_BY_ID =
+  LOCAL + "/controller/client_controller_update.php";
