938330fb16e73f7ad6eac9ab105245b20e09c271
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-07-12 22:31:54 -0300

    Adicionando url de client_by_id;

--- util/url.js
+++ util/url.js
@@ -4,3 +4,6 @@ export const LOCAL =
 export const FIND_ALL_CLIENT = LOCAL + "/controller/client_controller_list.php";
 
 export const CREATE_EQUIPMENT = LOCAL+"/controller/equipment_controller_insert.php"
+
+export const FIND_BY_ID_CLIENT =
+  LOCAL + "/controller/client_controller_by_id.php";
