f161700acb7b8c87a4bf29cfd553b1b7326ddfb4
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-07-25 22:33:10 -0300

    Adicionando Aba.

--- App.js
+++ App.js
@@ -4,13 +4,53 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import ListaDinamica from "./components/ListaDinamica";
 import FormClient from "./components/FormClient";
+import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import FormEquipment from "./components/FormEquipment";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+const Tab = createBottomTabNavigator();
 const Stack = createStackNavigator();
+
+function BottomNavegator() {
+  return (
+    <Tab.Navigator
+      initialRouteName={"Clientes"}
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+
+          if (route.name === "FormClient1") {
+            iconName = "home";
+            return <Icon name={iconName} color={color} size={size} />;
+          } else if (route.name === "Clientes") {
+            iconName = "view-dashboard";
+          } else {
+            iconName = "login";
+          }
+          return <Icon name={iconName} color={color} size={size} />;
+        },
+      })}
+    >
+      <Tab.Screen
+        name="Clientes"
+        component={ListaDinamica}
+        options={{ title: "", tabBarLabel: "Clientes" }}
+      />
+      <Tab.Screen
+        name="FormClient1"
+        component={FormClient}
+        options={{
+          title: "Controle de Os",
+          tabBarLabel: "Cadastro de Cliente",
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Clientes">
-        <Stack.Screen name="Clientes" component={ListaDinamica} />
+        <Stack.Screen name="Clientes" component={BottomNavegator} />
         <Stack.Screen
           name="FormClient"
           component={FormClient}
@@ -19,6 +59,12 @@ export default function App() {
         <Stack.Screen
           name="FormEquipment"
           component={FormEquipment}
+          options={{ headerShown: false }}
+        />
+
+        <Stack.Screen
+          name="Tela"
+          component={BottomNavegator}
           options={{ headerShown: false }}
         />
       </Stack.Navigator>
