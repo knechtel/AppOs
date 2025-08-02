c2e14c161f5a3b388bcf19d67492d3a8d05a2de7
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-08-02 20:00:03 -0300

    Adicionando o loading na página.

--- components/FormShow.js
+++ components/FormShow.js
@@ -1,29 +1,53 @@
-import {React,useEffect,useState} from "react";
-import { View, Image, StyleSheet,Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  View,
+  Image,
+  StyleSheet,
+  Button,
+  ActivityIndicator,
+} from "react-native";
 import { URL_PHOTO_CLIENT } from "../util/url";
 
 export default function FormShow({ route, navigation }) {
   const [id, setId] = useState();
   const [imageUrl, setImageUrl] = useState();
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const { id } = route.params;
     setId(id);
-    setImageUrl(URL_PHOTO_CLIENT + `${id}` + ".jpg");
-  }, [id]);
+    setImageUrl(URL_PHOTO_CLIENT + `${id}.jpg`);
+  }, []);
+
   const toTheClient = () => {
-    navigation.navigate("FormClient", { id: id });
+    navigation.navigate("FormClient", { id });
   };
+
   return (
     <View style={styles.container}>
-      <Image
-        source={{ uri: imageUrl }}
-        style={styles.image}
-        resizeMode="cover" // pode ser "contain", "stretch" etc.
-      />
-      <View style={styles.buttonContainer}>
-        <Button title="Voltar" onPress={toTheClient} />
-      </View>
+      {loading && (
+        <ActivityIndicator
+          size="large"
+          color="#6200ee"
+          style={{ marginBottom: 20 }}
+        />
+      )}
+
+      {imageUrl && (
+        <Image
+          source={{ uri: imageUrl }}
+          style={styles.image}
+          resizeMode="cover"
+          onLoadStart={() => setLoading(true)}
+          onLoadEnd={() => setLoading(false)}
+        />
+      )}
+
+      {!loading && (
+        <View style={styles.buttonContainer}>
+          <Button title="Voltar" onPress={toTheClient} />
+        </View>
+      )}
     </View>
   );
 }
@@ -38,5 +62,8 @@ const styles = StyleSheet.create({
     width: 200,
     height: 200,
     borderRadius: 10,
+  },
+  buttonContainer: {
+    marginTop: 20,
   },
 });
