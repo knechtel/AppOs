7f78cb040f3d8179b02939d4f53575f6d783a6a7
maiquelknechtel1 <maiquelknechtel@gmail.com>
2025-08-02 19:45:58 -0300

    Adicionando url de photo e formatando código.

--- components/FormShow.js
+++ components/FormShow.js
@@ -1,28 +1,29 @@
 import {React,useEffect,useState} from "react";
 import { View, Image, StyleSheet,Button } from "react-native";
+import { URL_PHOTO_CLIENT } from "../util/url";
 
-export default function FormShow({route,navigation}) {
-    const[id ,setId]=useState();
-    const[imageUrl, setImageUrl]=useState();
-  
+export default function FormShow({ route, navigation }) {
+  const [id, setId] = useState();
+  const [imageUrl, setImageUrl] = useState();
+
   useEffect(() => {
-      const { id } = route.params;
-      setId(id);
-      setImageUrl("https://sftcode.com/photo/"+`${id}`+".jpg");
-  },[id]);
-   const toTheClient = () => {
-     navigation.navigate("FormClient", { id: id });
-   };
+    const { id } = route.params;
+    setId(id);
+    setImageUrl(URL_PHOTO_CLIENT + `${id}` + ".jpg");
+  }, [id]);
+  const toTheClient = () => {
+    navigation.navigate("FormClient", { id: id });
+  };
   return (
     <View style={styles.container}>
-      <Image    
+      <Image
         source={{ uri: imageUrl }}
         style={styles.image}
         resizeMode="cover" // pode ser "contain", "stretch" etc.
       />
-         <View style={styles.buttonContainer}>
-                <Button title="Voltar" onPress={toTheClient}  />
-              </View>
+      <View style={styles.buttonContainer}>
+        <Button title="Voltar" onPress={toTheClient} />
+      </View>
     </View>
   );
 }
